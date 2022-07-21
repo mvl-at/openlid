b8d0671cb3c22f9fe36b274a0a847ff6fc2cf376
Richard Stöckl <richard.stoeckl@aon.at>
2022-07-21 16:19:29 +0200

    Implement paginiation on table

--- src/app/pages/archive/archive.component.ts
+++ src/app/pages/archive/archive.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ArchiveService} from '../../services/archive.service';
-import {Pagination, Score} from '../../common/archive';
+import {Score} from '../../common/archive';
+import {PageEvent} from '@angular/material/paginator';
 
 @Component({
   selector: 'lid-archive', templateUrl: './archive.component.html', styleUrls: ['./archive.component.scss']
@@ -8,10 +9,14 @@ import {Pagination, Score} from '../../common/archive';
 export class ArchiveComponent implements OnInit {
 
   displayedColumns: string[] = ['title', 'subtitles', 'composers', 'arrangers', 'publisher', 'location'];
-  limit: number = 30;
-  offset: number = 120;
+
+  // fields for the pagination
+  pageSizes = [10, 20, 30, 50, 100];
+  limit: number = 20;
+  skip: number = 0;
+  totalRows: number = 0;
+
   private viewScores: Score[] = [];
-  private result: Pagination<Score> | null = null;
 
   constructor(private archiveService: ArchiveService) {
   }
@@ -24,16 +29,22 @@ export class ArchiveComponent implements OnInit {
     return this.viewScores;
   }
 
-  get totalRows(): number {
-    return this.result?.total_rows ?? 0;
+  ngOnInit(): void {
+    this.refreshArchive();
   }
 
-  ngOnInit(): void {
-    this.archiveService.getAllScoresPaginated(this.limit, this.offset).subscribe({
+  changePage(event: PageEvent) {
+    this.skip = event.pageSize * event.pageIndex;
+    this.limit = event.pageSize;
+    this.refreshArchive();
+  }
+
+  private refreshArchive() {
+    this.archiveService.getAllScoresPaginated(this.limit, this.skip).subscribe({
       next: result => {
         this.viewScores = result.rows.map(r => r.doc);
+        this.totalRows = result.total_rows;
       }, error: console.log
     });
   }
-
 }
