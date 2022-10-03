6d23c2e3355b06acdd546b79143bc33e663d0194
Richard Stöckl <richard.stoeckl@aon.at>
2022-10-03 17:47:50 +0200

    Show error on failed login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,6 +5,7 @@ import {Login} from '../../common/login';
 import {SelfService} from '../../services/self.service';
 import {Router} from '@angular/router';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'lid-login', templateUrl: './login.component.html', styleUrls: ['./login.component.scss']
@@ -16,6 +17,8 @@ export class LoginComponent implements OnInit {
     password: this.formBuilder.nonNullable.control(''),
     persist: this.formBuilder.nonNullable.control(true),
   });
+
+  credentialsInvalid = false;
 
   constructor(private formBuilder: FormBuilder, private selfService: SelfService, private router: Router, private snackBar: MatSnackBar) {
   }
@@ -29,10 +32,19 @@ export class LoginComponent implements OnInit {
 
   login() {
     console.debug('login');
+    this.credentialsInvalid = false;
     this.selfService.login(this.loginForm.getRawValue()).subscribe({
       next: value => {
         console.debug('login succeeded', value);
         this.router.navigateByUrl('/self').then(() => this.snackBar.open('Sie sind nun angemeldet, Herzlich Willkommen!'));
+      }, error: (err: HttpErrorResponse) => {
+        console.debug('login failed', err);
+        if (err.status === 401) {
+          console.debug('invalid credentials');
+          this.credentialsInvalid = true;
+        } else {
+          this.snackBar.open('Unerwartetet Fehler bei der Anmeldung. Versuchen Sie es bitte später erneut. Kontaktieren Sie den Admninistrator sollte das Problem bestehen.');
+        }
       }
     });
   }
