ea8ae346aaac8d50d83772a526b5c1d36f778b67
Richard Stöckl <richard.stoeckl@aon.at>
2022-10-18 19:35:23 +0200

    Add snackbar service for http errors

---
+++ src/app/mat-helpers/http-error-snack-bar.service.spec.ts
@@ -0,0 +1,16 @@
+import {TestBed} from '@angular/core/testing';
+
+import {HttpErrorSnackBarService} from './http-error-snack-bar.service';
+
+describe('HttpErrorSnackBarService', () => {
+  let service: HttpErrorSnackBarService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(HttpErrorSnackBarService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
